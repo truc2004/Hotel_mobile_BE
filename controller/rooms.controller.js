76f2e148f0150ba7becb1af6484b1fe169979921
truc2004 <nguyenthanhtruc041204@gmail.com>
2025-10-27 13:53:00 +0700

    api room detail

--- controller/rooms.controller.js
+++ controller/rooms.controller.js
@@ -2,14 +2,25 @@ const Room = require('../models/rooms.model');
 
 module.exports.index = async (req, res) => {
     const result = await Room.find({});
-    const result2 = await Room.find({
-        _id: "68fed99bdae4abb6eb1bb4fd"
-    })
-
 
     res.json({
         res: result,
-        res2: result2
+    });
+
+
+}
+
+module.exports.roomDetail = async (req, res) => {
+    // const id = req.query.room_id;
+    const id = req.params.roomid;
+    console.log(id);
+
+    const result = await Room.find({
+        room_id: id
+    })
+
+    res.json({
+        res: result,
     });
 
 
--- routes/rooms.route.js
+++ routes/rooms.route.js
@@ -4,5 +4,7 @@ const route = express.Router();
 const controller = require("../controller/rooms.controller");
 
 route.get("/", controller.index);
+// route.get("/roomDetail", controller.roomDetail)
+route.get("/roomDetail/:roomid", controller.roomDetail)
 
 module.exports = route;
